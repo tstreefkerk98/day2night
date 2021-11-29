07ec5c697bce8b565c8d7a50e5673bc815a4930a
tstreefkerk <t.s.streefkerk@student.tudelft.nl>
2021-11-29 16:35:59 +0100

    recommit after `.gitignore` update

---
+++ utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+import random
+import torch
+import config
+from pathlib import Path
+
+
+def save_checkpoint(model, optimizer, filename=Path("my_checkpoint.pth.tar")):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def seed_everything(seed=42):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def get_images(train_folder):
+    return [Path(os.path.join(root, name)).resolve()
+            for root, _, files in os.walk(config.TRAIN_DIR + "/" + train_folder)
+            for name in files]
